c3e373fb266044173e9ba2a357e5ffbdb6e57bbb
Julian Krick <julian.krick@gmail.com>
2017-03-31 01:28:09 +0200

    Final Update and Jquery

--- gyno.js
+++ gyno.js
@@ -6,3 +6,34 @@ function myMap() {
   var marker = new google.maps.Marker({position:myCenter});
   marker.setMap(map);
 }
+
+$(document).ready(function() { 
+ $(".img-rounded").css('opacity', 0.7);
+ $("#MK").hover(function(){
+    $("#MK").fadeTo('fast', 3);
+    },
+    function(){
+        $("#MK").fadeTo('fast', 0.7);
+    });
+  $("#LS").hover(function(){
+    $("#LS").fadeTo('fast', 3);
+    },
+    function(){
+        $("#LS").fadeTo('fast', 0.7);
+    });
+
+   $("#SK").hover(function(){
+   	    $("#SK").fadeTo('fast', 3);
+    },
+    function(){
+        $("#SK").fadeTo('fast', 0.7);
+    });
+
+    $("#TV").hover(function(){
+   	    $("#TV").fadeTo('fast', 3);
+    },
+    function(){
+        $("#TV").fadeTo('fast', 0.7);
+    });
+
+});

557857ef98d95c13d4779aa622c100021754f63a
aidana12 <ai.mun1469@gmail.com>
2017-03-30 18:40:08 +0200

    js

---
+++ gyno.js
@@ -0,0 +1,8 @@
+function myMap() {
+  var myCenter = new google.maps.LatLng(51.4464952,6.6398927000000185);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}

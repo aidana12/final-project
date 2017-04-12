32fbed58318afe25382956dbb5e13dcb0c7bf701
aidana12 <ai.mun1469@gmail.com>
2017-04-12 02:02:02 +0200

    change of class and id names to more professional

--- gyno.js
+++ gyno.js
@@ -3,7 +3,6 @@ $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
-
 function myMap() {
   var myCenter = new google.maps.LatLng(51.4464952,6.6398927000000185);
   var mapCanvas = document.getElementById("map");
@@ -12,70 +11,59 @@ function myMap() {
   var marker = new google.maps.Marker({position:myCenter});
   marker.setMap(map);
 }
-
 $(document).ready(function() { 
- $(".img-rounded").css('opacity', 0.7);
- $("#MK").hover(function(){
+  $(".img-rounded").css('opacity', 0.7);
+  $("#MK").hover(function(){
     $("#MK").fadeTo('fast', 3);
-    },
-    function(){
-        $("#MK").fadeTo('fast', 0.7);
-    });
+  },
+  function(){
+    $("#MK").fadeTo('fast', 0.7);
+  });
+
   $("#LS").hover(function(){
     $("#LS").fadeTo('fast', 3);
-    },
-    function(){
-        $("#LS").fadeTo('fast', 0.7);
-    });
+  },
+  function(){
+    $("#LS").fadeTo('fast', 0.7);
+  });
 
-   $("#SK").hover(function(){
-   	    $("#SK").fadeTo('fast', 3);
-    },
-    function(){
-        $("#SK").fadeTo('fast', 0.7);
-    });
+  $("#SK").hover(function(){
+    $("#SK").fadeTo('fast', 3);
+  },
+  function(){
+    $("#SK").fadeTo('fast', 0.7);
+  });
 
-    $("#TV").hover(function(){
-   	    $("#TV").fadeTo('fast', 3);
-    },
-    function(){
-        $("#TV").fadeTo('fast', 0.7);
-    });
+  $("#TV").hover(function(){
+    $("#TV").fadeTo('fast', 3);
+  },
+  function(){
+    $("#TV").fadeTo('fast', 0.7);
+  });
 
-    $("#singlebutton").click(function(){
-    	console.log("clicked")
-    	$(window).scrollTo($("#nachricht"), 1000);
-
-    })
-
-    $("#uberu").click(function(){
-    	console.log("clicked")
-    	$(window).scrollTo($("#About"), 1000);
-
-    })
-
-    $("#teami").click(function(){
-    	console.log("clicked")
-    	$(window).scrollTo($("#Team"), 1000);
-
-    })
-
-   	$("#servizio").click(function(){
-   	console.log("clicked")
-   	$(window).scrollTo($("#Service"), 1000);
-
-    })
-
-   	$("#contatto").click(function(){
-   	console.log("clicked")
-   	$(window).scrollTo($("#Contact"), 1000);
-
-    })
-
-
-
-
-
-
+  $("#singlebutton").click(function(){
+   console.log("clicked")
+   $(window).scrollTo($("#nachricht"), 1000);
+ })
+  $("#uberu").click(function(){
+   console.log("clicked")
+   $(window).scrollTo($("#About"), 1000);
+ })
+  $("#teami").click(function(){
+   console.log("clicked")
+   $(window).scrollTo($("#Team"), 1000);
+ })
+  $("#servizio").click(function(){
+    console.log("clicked")
+    $(window).scrollTo($("#Services"), 1000);
+  })
+  $("#contatto").click(function(){
+    console.log("clicked")
+    $(window).scrollTo($("#Contact"), 1000);
+  })
+  $(".navbar-brand").click(function(){
+    console.log("clicked")
+    $(window).scrollTo($("#frauenarzt"), 1000);
+  })
 });
 
